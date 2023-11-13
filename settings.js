b1142696ec34e3d6dd70080347c62cc8f8a0ae3d
isurumd1 <150678681+isurumd1@users.noreply.github.com>
2023-11-13 18:08:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 
 // setting 
 global.autoread = true // auto read message
-global.autobio = false //auto ganti bio
+global.autobio = true //auto ganti bio
 global.anticall = true //anti call 
-global.userRegister = false
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.userRegister = true
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
@@ -32,24 +32,24 @@ global.language = english //change indonesia to english if you don't understand 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.ownername = "𝗠𝗥 𝗜𝗦𝗨𝗥𝗨 𝗖𝗠" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
 global.email = "mdsandaru2@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.premium = ['94766632281'] //premium user
+global.owner = ["766632281", "94766632281"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "766632281" // nomor wa kalian
+global.ownernomerr = "+94766632281" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
 global.packname = '© Alphabot-Md' //sticker wm ubah
 global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
-global.sessionName = 'session'
+global.sessionName = 'Raganork~2c16712011447bfefcae70e469ec643e:6e43314865334d7a7674773d'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
